5e689840fb11a9adeed594cebfe9e472a6e19e61
chedieck <estevao@lobo.dev.br>
2022-11-10 17:25:51 -0300

    feat: add example variables.py file

---
+++ variables.py
@@ -0,0 +1,8 @@
+TOKEN = '1234567890:s3crET7El3Gr4mBoT0k3Nd0N@tsh4r3zzzz'
+SURF_SKATE_URL = 'https://pr.olx.com.br/regiao-de-curitiba-e-paranagua/esportes-e-ginastica?q=surf%20skate'
+CHAT_ID_LIST = [123456789, 123456780]
+URL_SEARCH_LIST = [
+    # CAIXA_DE_SOM_URL,
+    # BICICLETA_URL
+    SURF_SKATE_URL
+]
