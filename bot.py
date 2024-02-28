17c37af40848f5e68da43209963a02c7ac543e42
chedieck <estevao@lobo.dev.br>
2024-02-27 22:56:54 -0300

    feat: MAJOR rework

--- bot.py
+++ bot.py
@@ -1,11 +1,16 @@
 from typing import List
-from core import Watcher
-from config import TOKEN, URL_TO_CHAT_DICT, SHOW_NEW_ON_START, SCAN_DELAY
-from telegram.ext import ApplicationBuilder, CommandHandler
+from core import Watcher, scan_logger, log_formatter
+from config import TOKEN, URL_TO_CHAT_DICT, SHOW_NEW_ON_START, SCAN_DELAY, LOG_LEVEL
+from telegram.ext import ApplicationBuilder, CommandHandler, CallbackContext
 from telegram import InputMediaPhoto, Update
 from telegram.constants import ParseMode
+from pathlib import Path
+from time import sleep
+
+import logging
 
 
+FLOOD_PREVENTIVE_DELAY_MS = 9000
 CHAT_ID_SET = set([
     chat_id for v in URL_TO_CHAT_DICT.values() for chat_id in v
 ])
@@ -13,7 +18,22 @@ URL_LIST = URL_TO_CHAT_DICT.keys()
 WATCHER_LIST = [
     Watcher(url) for url in URL_LIST
 ]
+ON = False
 
+
+## Configure logging
+Path("logs/").mkdir(exist_ok=True)
+
+app_logger = logging.getLogger(__name__)
+app_logger.setLevel(LOG_LEVEL)
+app_file_handler = logging.FileHandler('logs/full.log')
+app_file_handler.setLevel(LOG_LEVEL)
+app_file_handler.setFormatter(log_formatter)
+chat_logger = logging.getLogger('chat')
+chat_logger.setLevel(LOG_LEVEL)
+chat_file_handler = logging.FileHandler('logs/chat.log')
+chat_file_handler.setFormatter(log_formatter)
+chat_logger.addHandler(chat_file_handler)
 
 def _pre_parse_html(text: str):
     return text.replace('<br>', '')
@@ -31,19 +51,34 @@ def make_album(url_arr, caption):
 
 async def send_album(context, chat_id: int, pic_arr: List):
     for i in range(0, len(pic_arr), 10):
+        if (i > 0):
+            sleep(FLOOD_PREVENTIVE_DELAY_MS/1000)
         await context.bot.send_media_group(chat_id, media=pic_arr[i: i+10])
 
 async def print_last_if_changed(context, w: Watcher):
-    if (new_ads:= w.update()):
-        for new_ad in new_ads:
+    if (new_ads:= await w.update()):
+        subscribed_users = URL_TO_CHAT_DICT[w.url]
+        chat_logger.info(f'Sending {len(new_ads):^3} ads to {len(subscribed_users):^3} users...')
+        chat_logger.debug(f'{new_ads=}\n{subscribed_users=}')
+        for idx, new_ad in enumerate(new_ads):
             caption = _pre_parse_html(str(new_ad))
+            chat_logger.info(f'— Ad: {new_ad.title} ({len(new_ad.image_url_list):^2}) pics to users {subscribed_users}')
+            chat_logger.debug(f'{new_ad=}')
             pic_arr = make_album(new_ad.image_url_list, caption)
-            for chat_id in URL_TO_CHAT_DICT[w.url]:
+            for chat_id in subscribed_users:
                 await send_album(context, chat_id, pic_arr)
+            sleep(FLOOD_PREVENTIVE_DELAY_MS/1000)
+        chat_logger.info(f'Finished sending ads to users')
+    else:
+        scan_logger.info(f'No new ads for {w}')
 
 
 async def watch_job(context):
+    scan_logger.info(f'Scanning {len(WATCHER_LIST)} URLs...')
     for w in WATCHER_LIST:
+        scan_logger.info(f'— Watcher URL: {w.url}')
+        scan_logger.debug(f'{w.seen=}')
+        scan_logger.debug(f'{w.hash=}')
         await print_last_if_changed(context, w)
 
 async def stop(update, context):
@@ -53,24 +88,33 @@ async def stop(update, context):
         for job in current_jobs:
             job.schedule_removal()
 
-async def start(update: Update, context):
-    user_id = update.message['chat']['id']
+async def start(update: Update, context: CallbackContext):
+    global ON
+    chat = update.message.chat
+    user_id = chat.id
+    username = chat.username
+    first_name = chat.first_name
+    last_name = chat.last_name
+
     if user_id not in CHAT_ID_SET:
-        print(f'User of id {user_id} tried starting the bot.')
+        chat_logger.warn(f'User {user_id} tried starting the bot!')
+        chat_logger.debug(f'{new_ads=}\n{subscribed_users=}')
         await update.message.reply_text('010010110101010101010110101000101010111010101011011101010110111100010110')
 
     else:
-        await update.message.reply_text('Ativado.')
-        chat = update.message.chat
-        print(f"Ativado para usuário @{chat.username} ({chat.first_name} {chat.last_name})")
+        await update.message.reply_text(f'<b>{first_name} {last_name}</b>\n@{username}\n<i>#{user_id}</i>\n\nAtivado.', parse_mode=ParseMode.HTML)
+        chat_logger.info(f"Ativado para usuário ({first_name} {last_name}) @{username} #{user_id}")
         if not SHOW_NEW_ON_START:
-            [w.update() for w in WATCHER_LIST]
-        context.job_queue.run_repeating(
-            watch_job,
-            SCAN_DELAY,
-            0,
-            name=f'{user_id}-watcher'
-        )
+            [await w.update() for w in WATCHER_LIST]
+        if not ON:
+            job = context.job_queue.run_repeating(
+                watch_job,
+                SCAN_DELAY,
+                name=f'{user_id}-watcher'
+            )
+            await job.run(context.application)
+            ON = True
+
 
 
 if __name__ == '__main__':
--- core.py
+++ core.py
@@ -1,9 +1,15 @@
 from bs4 import BeautifulSoup
 from bs4.element import NavigableString, Tag
-from typing import List
+from typing import List, Optional
+from concurrent.futures import ThreadPoolExecutor
 import subprocess
 import json
+import asyncio
 import hashlib
+import logging
+from pathlib import Path
+import json
+
 
 
 CURL_ARGS = str("""--compressed"""
@@ -19,12 +25,39 @@ CURL_ARGS = str("""--compressed"""
 
 
 MAX_DESCIPTION_SIZE = 600
+NEW_ADS_LIMIT = 10
+
+
+Path("logs/").mkdir(exist_ok=True)
+log_format_string = '[%(name)-4s][%(levelname)8s](%(asctime)s): %(message)s'
+log_formatter = logging.Formatter(log_format_string)
+logging.basicConfig(
+    level=logging.ERROR,
+    format=log_format_string,
+)
+
+scan_logger = logging.getLogger('scan')
+scan_logger.setLevel(LOG_LEVEL)
+scan_file_handler = logging.FileHandler('logs/scan.log')
+scan_file_handler.setFormatter(log_formatter)
+scan_logger.addHandler(scan_file_handler)
+
 
 def curl_request(url):
     curl_command = f'curl \'{url}\' {CURL_ARGS}'
     process = subprocess.Popen(curl_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out, err = process.communicate()
     return out
+
+
+async def async_curl_request(url):
+    loop = asyncio.get_running_loop()
+    with ThreadPoolExecutor() as pool:
+        curl_command = f'curl \'{url}\' {CURL_ARGS}'
+        # Use run_in_executor to run the blocking function in a separate thread
+        result = await loop.run_in_executor(pool, lambda: subprocess.run(curl_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True))
+        return result.stdout
+
 
 
 class Ad():
@@ -72,38 +105,135 @@ class Ad():
         self.municipio = ''
         self.bairro = ''
         self.description = ''
+        self.imovelData = {}
+        self.phone = None
+        self.zipcode = None
+        self.condominio = None
+        self.garage_spaces = None
+        self.bathrooms = None
+        self.rooms = None
+        self.size = None
+        self.iptu = None
+        self.full_price = None
+        self.full_label = None
 
-    def update_detailed_data(self):
+    async def update_detailed_data(self):
         """Update the Ad's location & description data, which is not present in the `raw_ad_list`
         constructor parameter.
+        Available properties 27-02-2024:
+            'adId',
+            'listId',
+            'body',
+            'subject',
+            'priceLabel',
+            'priceValue',
+            'oldPrice',
+            'professionalAd',
+            'category',
+            'parentCategoryName',
+            'categoryName',
+            'searchCategoryLevelZero',
+            'searchCategoryLevelOne',
+            'searchCategoryLevelTwo',
+            'origListTime',
+            'adReply',
+            'planBundleZap',
+            'friendlyUrl',
+            'hasRealEstateHighlight',
+            'loanSpecificData',
+            'user',
+            'phone',
+            'images',
+            'videos',
+            'location',
+            'vehicleSpecificData',
+            'properties',
+            'pubSpecificData',
+            'trackingSpecificData',
+            'searchboxes',
+            'breadcrumbUrls',
+            'featured',
+            'carSpecificData',
+            'abuyFipePrice',
+            'abuyPriceRef',
+            'realEstateSpecificData',
+            'olxPay',
+            'olxDelivery',
+            'vehicleReport',
+            'vehicleTags',
+            'sellerHistory',
+            'description',
+            'price',
+            'listTime',
+            'locationProperties',
+            'securityTips',
+            'denounceLink',
+            'nativeVas',
+            'isFeatured',
+            'chatEnabled',
+            'isCompetitionLock',
+            'signedPostalCode',
+            'slotsId'
         """
         if not self.url:
             return
-        page_content = curl_request(self.url)
+        page_content = await async_curl_request(self.url)
         soup = BeautifulSoup(page_content, 'html.parser')
         scripts = soup.findAll('script')
 
         try:
             next_data = next((s for s in scripts if s.has_attr('data-json')), None)
             if next_data == None:
-                print("No data-json.")
+                scan_logger.error("No data-json.")
                 return
             data_json = json.loads(next_data['data-json'])
         except TypeError:
-            print(f'Type error for ad of URL {self.url}')
+            scan_logger.error(f'Type error for ad of URL {self.url}')
             return
 
-        location_properties = data_json['ad']['locationProperties']
+        ad_data = data_json.get('ad')
+        location_properties = ad_data.get('locationProperties')
 
-        for prop in location_properties:
-            if prop['label'] == 'CEP':
-                self.cep = prop['value']
-            if prop['label'] == 'Município':
-                self.municipio = prop['value']
-            if prop['label'] == 'Bairro':
-                self.bairro = prop['value']
+        phone_data = ad_data.get('phone')
+        phone = phone_data.get("phone") if phone_data else None
+        self.phone = phone
 
-        self.description = data_json['ad']['description']
+        location = ad_data.get('location')
+        zipcode = location.get('zipcode') if location else None
+        self.zipcode = zipcode
+
+        properties = ad_data.get('properties')
+        if properties:
+            for prop in properties:
+                name = prop.get('name')
+                value = prop.get('value')
+                if name == 'condominio':
+                    self.condominio = value
+                if name == 'garage_spaces':
+                    self.garage_spaces = value
+                if name == 'bathrooms':
+                    self.bathrooms = value
+                if name == 'rooms':
+                    self.rooms = value
+                if name == 'size':
+                    self.size = value
+                if name == 'iptu':
+                    self.iptu = value
+                if name == 're_rent_full_price':
+                    label = prop.get('label')
+                    self.full_price = value
+                    self.full_label = label
+        if location_properties:
+            for prop in location_properties:
+                if prop['label'] == 'CEP':
+                    self.cep = prop['value']
+                if prop['label'] == 'Município':
+                    self.municipio = prop['value']
+                if prop['label'] == 'Bairro':
+                    self.bairro = prop['value']
+
+        scan_logger.info(f'——— Got detailed data for {self.title}')
+        self.description = ad_data['description']
 
     def __eq__(self, other):
         """Two ads are equal if their hash property is the same.
@@ -111,7 +241,10 @@ class Ad():
         return self.hash == other.hash if other is not None else False
 
     def __repr__(self):
-        price_segment = f"Preço: <b>R$ {self.price}</b>\n"
+        if self.full_label and self.full_price:
+            price_segment = f"{self.full_label}: <b>R$ {self.full_price}</b>\n"
+        else: 
+            price_segment = f"Preço: <b>R$ {self.price}</b>\n"
         if self.old_price:
             price_segment = price_segment.rstrip('\n') + f" (de {self.old_price})\n"
 
@@ -119,12 +252,26 @@ class Ad():
         if len(description) > MAX_DESCIPTION_SIZE:
             description = self.description[:MAX_DESCIPTION_SIZE - 3] + '...'
 
-
+        zip_code_text = f' <b>{self.zipcode}</b\n>'if self.zipcode else ''
+        phone_info = f'Telefone: <b>{self.phone}</b\n>'if self.phone else ''
+        price_info = f'(Aluguel: <b>{self.price}</b> Condomínio: <b>{self.condominio}</b> IPTU: <b>{self.iptu}</b>)\n' if self.condominio or self.iptu else ''
+        imovel_info = (
+            '<i>-------I M Ó V E L {--------\n</i>'
+            + price_info +
+            f'- Vagas pra garagem: {self.garage_spaces}\n'
+            f'- Banheiros: {self.bathrooms}\n'
+            f'- Quartos: {self.rooms}\n'
+            f'- Área útil: {self.size}\n'
+            '<i>-----} I M Ó V E L----------\n</i>'
+        ) if self.rooms else ''
         return (
             f"<b>{self.title:^45}</b>\n\n"
-            + f"Lugar: <i>{self.municipio} - {self.bairro}</i>\n"
+            + f"Lugar: <i>{self.municipio} - {self.bairro}</i>{zip_code_text}\n"
+            + phone_info
+            + imovel_info
             + price_segment
-            + f"Descrição: <i>{description}</i>\n\n"
+            + "Descrição:\n"
+            + f"- {description}\n\n"
             + f'<a href="{self.url}">URL</a>'
         )
 
@@ -166,6 +313,16 @@ class Watcher():
 
         self.seen = set()
 
+    def __repr__(self):
+        """
+        Parameters
+        ----------
+
+        url
+            URL to be watched.
+        """
+        return self.url.split('.com.br')[1]
+
 
     @classmethod
     def get_ad_list_hash(cls, ad_list: List[Ad] | None):
@@ -190,13 +347,21 @@ class Watcher():
             elif type(data) == NavigableString:
                 data_string = str(data)
             else:
-                print('Data is actually', type(data))
+                scan_logger.error(f'Data is actually {type(data)}')
                 return []
             data_json = json.loads(data_string)
             return data_json['props']['pageProps']['ads']
         return []
 
-    def update(self) -> List[Ad] | None:
+
+    async def _update_ad_detailed_data(self, ad: Ad) -> Optional[Ad]:
+        await ad.update_detailed_data()  # Ensure this is an async method
+        if ad.hash not in self.seen:
+            self.seen.add(ad.hash)
+            return ad
+        return None
+
+    async def update(self) -> List[Ad] | None:
         """Updates `self.ad_list` and `self.last_ad`.
 
         Returns
@@ -208,12 +373,20 @@ class Watcher():
         soup = BeautifulSoup(page_content, 'html.parser')
         ad_list = Watcher.get_ad_list(soup)
         new_ad_list = [Ad(raw_ad) for raw_ad in ad_list if 'subject' in raw_ad.keys()]
+        new_ad_list = new_ad_list[:NEW_ADS_LIMIT]
         if Watcher.get_ad_list_hash(new_ad_list) != self.hash:
-            ret = []
-            for new_ad in new_ad_list:
+            tasks = []
+            scan_logger.info(f'—— Preparing {len(new_ad_list)} requests for detailed data...')
+            for idx, new_ad in enumerate(new_ad_list):
                 if new_ad.hash not in self.seen:
-                    new_ad.update_detailed_data()
-                    self.seen.add(new_ad.hash)
-                    ret.append(new_ad)
+                    scan_logger.info(f'——— Will create request for unseen add #{idx:<2} — {new_ad.title}')
+                    task = self._update_ad_detailed_data(new_ad)
+                    tasks.append(task)
+
+            scan_logger.info(f'—— Gathering detailed data...')
+            updated_ads = await asyncio.gather(*tasks)
+            ret = [ad for ad in updated_ads if ad]
+            scan_logger.info(f'Finished sending requests, updated {len(ret)} ads.')
+
             self.ad_list = new_ad_list
             return ret
